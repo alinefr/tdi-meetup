a24e4e394a29a394304d65035058079027370c27
Aline Freitas <aline@alinefreitas.com.br>
2017-09-27 13:47:47 -0300

    After meetup commit.

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description 'Installs/Configures meetup'
 long_description 'Installs/Configures meetup'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+depends 'docker', '~> 2.16.0'
+depends 'docker_compose', '~> 0.1.1'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,30 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+# Service docker installed, enabled and running
+docker_service 'default' do
+  action [:create, :start]
+end
+
+# Nginx container image
+include_recipe 'docker_compose::installation'
+
+directory '/srv/docker/hello/html' do
+  recursive true
+end
+
+cookbook_file '/srv/docker/hello/docker-compose.yml' do
+  source 'docker-compose.yml'
+  notifies :up, 'docker_compose_application[nginx]', :delayed
+end
+
+docker_compose_application 'nginx' do
+  action :up
+  compose_files ['/srv/docker/hello/docker-compose.yml']
+end
+
+# Serve html page containing Hello World
+cookbook_file '/srv/docker/hello/html/index.html' do
+  source 'index.html'
+end
